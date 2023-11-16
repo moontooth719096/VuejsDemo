ea988dd9eff0bdc04c424f516a0b21485c3dd5a7
KevinWu <KevinWu@wits.com>
2023-11-16 16:47:49 +0800

    聊天室基本完成

--- DemoProgressBarAPI/Models/ChatRoom/ChatUser.cs
+++ DemoProgressBarAPI/Models/ChatRoom/ChatUser.cs
@@ -5,7 +5,7 @@
         public string UserName { get; set; }
         public string UserId {  get; set; }
         public string ConnectionID {  get; set; }
-        public string imgPath => "https://fakeimg.pl/60/";
+        public string imgPath => "https://fakeimg.pl/80/";
         public int NoReadCount { get; set; }
         public string LastMesage { get; set; }
     }
