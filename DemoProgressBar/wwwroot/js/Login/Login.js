745e195806858d862fdb4529b9f4416789457005
KevinWu <KevinWu@wits.com>
2023-12-19 17:47:21 +0800

    修改 GoogleOAuth 改成Vue.js

--- DemoProgressBar/wwwroot/js/Login/Login.js
+++ DemoProgressBar/wwwroot/js/Login/Login.js
@@ -1,147 +1,276 @@
-﻿//const vm = Vue.createApp({
-//    data() {
-//        return {
-//        }
-//    },
-//    methods: {
-//        onSuccess(googleUser) {
-//            console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
-//        },
-//        onFailure(error) {
-//            console.log(error);
-//        },
-//        renderButton() {
-//            gapi.signin2.render('my-signin2', {
-//                'scope': 'profile email',
-//                'width': 240,
-//                'height': 50,
-//                'longtitle': true,
-//                'theme': 'dark',
-//                'onsuccess': onSuccess,
-//                'onfailure': onFailure
-//            });
-//        }
-//    }
-//}).mount('#app')
-window.onload = function () {
-    checkLoginStatus();
-}
+﻿
+const vm = Vue.createApp({
+    data() {
+        return {
+            ClientID: window.ClientID
+            //google: self.google
+        }
+    },
+    created() {
+        
+    },
+    mounted() {
+        this.checkLoginStatus();
+    },
+    methods: {
+        //確認登入狀況
+        checkLoginStatus() {
+            google.accounts.id.disableAutoSelect();
+            this.deleteCookie("token");
+            this.deleteCookie("username");
+            this.deleteCookie("email");
+            this.deleteCookie("userInfo");
+            /*signoutItemDisplay(false);*/
+            var userCookie = this.getCookie("token");
 
+            if (userCookie) {
+                this.signoutItemDisplay(true);
 
-function checkLoginStatus() {
-        google.accounts.id.disableAutoSelect();
-        deleteCookie("token");
-        deleteCookie("username");
-        deleteCookie("email");
-        deleteCookie("userInfo");
-        /*signoutItemDisplay(false);*/
-    var userCookie = getCookie("token");
+                // go to new page
+                //window.location.replace("/Home/List");
 
-    if (userCookie) {
-        signoutItemDisplay(true);
+            } else {
 
-        // go to new page
-        //window.location.replace("/Home/List");
+                this.signoutItemDisplay(false);
+                google.accounts.id.initialize({
+                    client_id: this.ClientID,
+                    callback: this.handleCredentialResponse
+                });
 
-    } else {
+                google.accounts.id.renderButton(
+                    document.getElementById("buttonDiv"),
+                    { type: "standard", shape: "rectangular", theme: "outline", size: "medium", text: "signin_with", logo_alignment: "left" }  // customization attributes
+                );
 
-        signoutItemDisplay(false);
+                google.accounts.id.prompt(); // also display the One Tap dialog
+            }
+        },
+        //取得cookie中的token
+        getCookie(cookieName) {
+            var name = cookieName + "=";
+            var decodedCookie = decodeURIComponent(document.cookie);
+            var cookieArray = decodedCookie.split(';');
 
-        //google.accounts.id.initialize({
-        //    client_id: window.ClientID,
-        //    login_uri: 'https://localhost:7068/api/GoogleAuth/Login',
-        //    context: 'signin',
-        //    ux_mode: "popup",
-        //    //callback: handleCredentialResponse,
-        //    auto_select: true,
-        //    use_fedcm_for_prompt: true,
-        //    auto_prompt: true
-        //});
+            for (var i = 0; i < cookieArray.length; i++) {
+                var cookie = cookieArray[i].trim();
+                if (cookie.indexOf(name) === 0) {
+                    return cookie.substring(name.length, cookie.length);
+                }
+            }
+            return null;
+        },
+        //控制登出按鈕顯示
+        signoutItemDisplay(display) {
+            var signoutItem = document.getElementById("signout_item");
+            if (signoutItem) {
+                if (display) {
+                    signoutItem.style.display = "inline";
+                } else {
+                    signoutItem.style.display = "none";
+                }
+            }
+        },
+        handleCredentialResponse(response) {
 
-        google.accounts.id.renderButton(
-            document.getElementById("g_id_signin"),
-            { theme: "outline", size: "large" }  // customization attributes
-        );
+            const responsePayload = this.decodeJwtResponse(response.credential);
+            //console.log("ID: " + responsePayload.sub);
+            //console.log('Full Name: ' + responsePayload.name);
+            ////console.log('Given Name: ' + responsePayload.given_name);
+            ////console.log('Family Name: ' + responsePayload.family_name);
+            //console.log("Image URL: " + responsePayload.picture);
+            //console.log("Email: " + responsePayload.email);
+            //console.log("Encoded JWT ID token: " + response.credential);
 
-        google.accounts.id.prompt(); // also display the One Tap dialog
+            // set up to cookie
+            var expirationDate = new Date();
+            var usernameCookie = `username=${responsePayload.given_name}${responsePayload.family_name}; path=/`;
+            expirationDate.setDate(expirationDate.getDate() + 14);
+            usernameCookie += "; expires=" + expirationDate.toUTCString() + " ; secure ;samesite=strict";
+            document.cookie = usernameCookie
 
-    }
-}
+            var emailCookie = "email=" + responsePayload.email.toString() + "; path=/";
+            expirationDate.setDate(expirationDate.getDate() + 14);
+            emailCookie += "; expires=" + expirationDate.toUTCString() + " ; secure ;samesite=strict";
+            document.cookie = emailCookie
 
-// After login success 
-function handleCredentialResponse(response) {
+            var tokenCookie = "token=" + response.credential + "; path=/";
+            expirationDate.setDate(expirationDate.getDate() + 14);
+            tokenCookie += "; expires=" + expirationDate.toUTCString() + " ; secure ;samesite=strict";
+            document.cookie = tokenCookie
 
-    const responsePayload = decodeJwtResponse(response.credential);
-    //console.log("ID: " + responsePayload.sub);
-    //console.log('Full Name: ' + responsePayload.name);
-    ////console.log('Given Name: ' + responsePayload.given_name);
-    ////console.log('Family Name: ' + responsePayload.family_name);
-    //console.log("Image URL: " + responsePayload.picture);
-    //console.log("Email: " + responsePayload.email);
-    //console.log("Encoded JWT ID token: " + response.credential);
+            var userData = {
+                username: `${responsePayload.given_name}${responsePayload.family_name}`,
+                email: responsePayload.email,
+                token: response.credential
+            };
 
-    // set up to cookie
-    var expirationDate = new Date();
-    var usernameCookie = `username=${responsePayload.given_name}${responsePayload.family_name}; path=/`;
-    expirationDate.setDate(expirationDate.getDate() + 14);
-    usernameCookie += "; expires=" + expirationDate.toUTCString() + " ; secure ;samesite=strict";
-    document.cookie = usernameCookie
+            var userCookie = `userInfo=${JSON.stringify(userData)}; path=/;`;
+            expirationDate.setDate(expirationDate.getDate() + 14);
+            userCookie += `expires=${expirationDate.toUTCString()}; secure; samesite=strict`;
+            document.cookie = userCookie;
 
-    var emailCookie = "email=" + responsePayload.email.toString() + "; path=/";
-    expirationDate.setDate(expirationDate.getDate() + 14);
-    emailCookie += "; expires=" + expirationDate.toUTCString() + " ; secure ;samesite=strict";
-    document.cookie = emailCookie
+            // go to new page
+            //window.location.replace("https://localhost:7068/WeatherForecast");
 
-    var tokenCookie = "token=" + response.credential + "; path=/";
-    expirationDate.setDate(expirationDate.getDate() + 14);
-    tokenCookie += "; expires=" + expirationDate.toUTCString() + " ; secure ;samesite=strict";
-    document.cookie = tokenCookie
+        },
+        decodeJwtResponse(token) {
+            var base64Url = token.split(".")[1];
+            var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+            var jsonPayload = decodeURIComponent(
+                atob(base64)
+                    .split("")
+                    .map(function (c) {
+                        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+                    })
+                    .join("")
+            );
 
-    var userData = {
-        username: `${responsePayload.given_name}${responsePayload.family_name}`,
-        email: responsePayload.email,
-        token: response.credential
-    };
+            return JSON.parse(jsonPayload);
+        },
+        logOut() {
+            google.accounts.id.disableAutoSelect();
+            this.deleteCookie("token");
+            this.deleteCookie("username");
+            this.deleteCookie("email");
+            this.deleteCookie("userInfo");
+            this.signoutItemDisplay(false);
+            //window.location.replace("/Home/Index");
+        },
+        deleteCookie(cookieName) {
+            var cookies = document.cookie.split(";");
 
-    var userCookie = `userInfo=${JSON.stringify(userData)}; path=/;`;
-    expirationDate.setDate(expirationDate.getDate() + 14);
-    userCookie += `expires=${expirationDate.toUTCString()}; secure; samesite=strict`;
-    document.cookie = userCookie;
-
-    // go to new page
-    window.location.replace("https://localhost:7068/WeatherForecast");
-
-}
-
-function getCookie(cookieName) {
-    var name = cookieName + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var cookieArray = decodedCookie.split(';');
-
-    for (var i = 0; i < cookieArray.length; i++) {
-        var cookie = cookieArray[i].trim();
-        if (cookie.indexOf(name) === 0) {
-            return cookie.substring(name.length, cookie.length);
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = cookies[i];
+                var eqPos = cookie.indexOf("=");
+                var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+                if (name.trim() === cookieName) {
+                    document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+                    break;
+                }
+            }
         }
     }
-    return null;
-}
+}).mount('#app')
+
+//window.onload = function () {
+//    checkLoginStatus();
+//}
+
+
+//function checkLoginStatus() {
+//        google.accounts.id.disableAutoSelect();
+//        deleteCookie("token");
+//        deleteCookie("username");
+//        deleteCookie("email");
+//        deleteCookie("userInfo");
+//        /*signoutItemDisplay(false);*/
+//    var userCookie = getCookie("token");
+
+//    if (userCookie) {
+//        signoutItemDisplay(true);
+
+//        // go to new page
+//        //window.location.replace("/Home/List");
+
+//    } else {
+
+//        signoutItemDisplay(false);
+
+//        //google.accounts.id.initialize({
+//        //    client_id: window.ClientID,
+//        //    login_uri: 'https://localhost:7068/api/GoogleAuth/Login',
+//        //    context: 'signin',
+//        //    ux_mode: "popup",
+//        //    //callback: handleCredentialResponse,
+//        //    auto_select: true,
+//        //    use_fedcm_for_prompt: true,
+//        //    auto_prompt: true
+//        //});
+
+//        google.accounts.id.renderButton(
+//            document.getElementById("g_id_signin"),
+//            { theme: "outline", size: "large" }  // customization attributes
+//        );
+
+//        google.accounts.id.prompt(); // also display the One Tap dialog
+
+//    }
+//}
+
+// After login success 
+//function handleCredentialResponse(response) {
+
+//    const responsePayload = decodeJwtResponse(response.credential);
+//    //console.log("ID: " + responsePayload.sub);
+//    //console.log('Full Name: ' + responsePayload.name);
+//    ////console.log('Given Name: ' + responsePayload.given_name);
+//    ////console.log('Family Name: ' + responsePayload.family_name);
+//    //console.log("Image URL: " + responsePayload.picture);
+//    //console.log("Email: " + responsePayload.email);
+//    //console.log("Encoded JWT ID token: " + response.credential);
+
+//    // set up to cookie
+//    var expirationDate = new Date();
+//    var usernameCookie = `username=${responsePayload.given_name}${responsePayload.family_name}; path=/`;
+//    expirationDate.setDate(expirationDate.getDate() + 14);
+//    usernameCookie += "; expires=" + expirationDate.toUTCString() + " ; secure ;samesite=strict";
+//    document.cookie = usernameCookie
+
+//    var emailCookie = "email=" + responsePayload.email.toString() + "; path=/";
+//    expirationDate.setDate(expirationDate.getDate() + 14);
+//    emailCookie += "; expires=" + expirationDate.toUTCString() + " ; secure ;samesite=strict";
+//    document.cookie = emailCookie
+
+//    var tokenCookie = "token=" + response.credential + "; path=/";
+//    expirationDate.setDate(expirationDate.getDate() + 14);
+//    tokenCookie += "; expires=" + expirationDate.toUTCString() + " ; secure ;samesite=strict";
+//    document.cookie = tokenCookie
+
+//    var userData = {
+//        username: `${responsePayload.given_name}${responsePayload.family_name}`,
+//        email: responsePayload.email,
+//        token: response.credential
+//    };
+
+//    var userCookie = `userInfo=${JSON.stringify(userData)}; path=/;`;
+//    expirationDate.setDate(expirationDate.getDate() + 14);
+//    userCookie += `expires=${expirationDate.toUTCString()}; secure; samesite=strict`;
+//    document.cookie = userCookie;
+
+//    // go to new page
+//    window.location.replace("https://localhost:7068/WeatherForecast");
+
+//}
+
+//function getCookie(cookieName) {
+//    var name = cookieName + "=";
+//    var decodedCookie = decodeURIComponent(document.cookie);
+//    var cookieArray = decodedCookie.split(';');
+
+//    for (var i = 0; i < cookieArray.length; i++) {
+//        var cookie = cookieArray[i].trim();
+//        if (cookie.indexOf(name) === 0) {
+//            return cookie.substring(name.length, cookie.length);
+//        }
+//    }
+//    return null;
+//}
 
 // Tool
-function decodeJwtResponse(token) {
-    var base64Url = token.split(".")[1];
-    var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-    var jsonPayload = decodeURIComponent(
-        atob(base64)
-            .split("")
-            .map(function (c) {
-                return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-            })
-            .join("")
-    );
+//function decodeJwtResponse(token) {
+//    var base64Url = token.split(".")[1];
+//    var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+//    var jsonPayload = decodeURIComponent(
+//        atob(base64)
+//            .split("")
+//            .map(function (c) {
+//                return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+//            })
+//            .join("")
+//    );
 
-    return JSON.parse(jsonPayload);
-}
+//    return JSON.parse(jsonPayload);
+//}
 
 // logout
 //const button = document.getElementById('g_id_signin');
@@ -156,27 +285,27 @@ function decodeJwtResponse(token) {
 //    window.location.replace("/Home/Index");
 //}
 
-function deleteCookie(cookieName) {
-    var cookies = document.cookie.split(";");
+//function deleteCookie(cookieName) {
+//    var cookies = document.cookie.split(";");
 
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        var eqPos = cookie.indexOf("=");
-        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        if (name.trim() === cookieName) {
-            document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-            break;
-        }
-    }
-}
+//    for (var i = 0; i < cookies.length; i++) {
+//        var cookie = cookies[i];
+//        var eqPos = cookie.indexOf("=");
+//        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+//        if (name.trim() === cookieName) {
+//            document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+//            break;
+//        }
+//    }
+//}
 
-function signoutItemDisplay(display) {
-    var signoutItem = document.getElementById("signout_item");
-    if (signoutItem) {
-        if (display) {
-            signoutItem.style.display = "inline";
-        } else {
-            signoutItem.style.display = "none";
-        }
-    }
-}
+//function signoutItemDisplay(display) {
+//    var signoutItem = document.getElementById("signout_item");
+//    if (signoutItem) {
+//        if (display) {
+//            signoutItem.style.display = "inline";
+//        } else {
+//            signoutItem.style.display = "none";
+//        }
+//    }
+//}
